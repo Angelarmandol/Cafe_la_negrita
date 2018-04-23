961e0695d7eea533a8ab6dc8918e8274e63b8ee8
Angel Lopez <angelarmandol@hotmail.com>
2018-04-23 11:50:17 -0700

    ventanas
    control

--- Eclipse Sinc/Proyecto Eclipse WB/src/paquetaxo/Inicio.java
+++ Eclipse Sinc/Proyecto Eclipse WB/src/paquetaxo/Inicio.java
@@ -154,3 +154,14 @@ public class Inicio {
 		frame.getContentPane().add(lblLogo);
 	}
 }
+/**
+ * 
+ * "Por que las mentes agudas que
+ * han inventado una máquina que haga
+ * el trabajo de 60 hombres, deben
+ * ahora encontrar la forma de conservar
+ * y buscar trabajo a los desplazados."
+ * 
+ * Robert O'Brien | Colexion cientifica de TimeLife |
+ *  Máquinas | 1979 | Pagina 188
+ */
