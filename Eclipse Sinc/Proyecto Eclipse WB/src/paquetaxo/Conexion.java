01b825ac8df551ad3d3886fb1cc27d7ec0b97100
Angel Lopez <angelarmandol@hotmail.com>
2018-05-22 09:59:12 -0700

    cambio de conexion por mysql
    puto

--- Eclipse Sinc/Proyecto Eclipse WB/src/paquetaxo/Conexion.java
+++ Eclipse Sinc/Proyecto Eclipse WB/src/paquetaxo/Conexion.java
@@ -1,72 +1,174 @@
 package paquetaxo;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import javax.swing.JOptionPane;
 
 public class Conexion {
 
-	public Connection conexion;
-	public Statement sentencia;
-	private final String controlador;
-	private final String nombre_bd;
-	private final String usuarioBD;
-	private final String passwordBD;
 
-	public Conexion() {
 
-		this.controlador = "sun.jdbc.odbc.JdbcOdbcDriver";
-		this.nombre_bd = "C:\\Users\\Inspiron3647\\Documents\\GitHub\\Cafe_la_negrita\\Eclipse Sinc\\Database1.accdb";
-		this.usuarioBD = "";
-		this.passwordBD = "";
+	public static Connection getConnection() {
 
-	}
-
-	public boolean EstablecerConexion() throws SQLException {
-
+		String user = "Angel";
+		String password = "muu295t";
+		String ruta = "jdbc:mysql://localhost/proyecto";
+	 
 		try {
+			Class.forName("com.mysql.jdbc.Driver"); // se carga el driver
 
-			conexion = DriverManager.getConnection("jdbc:ucanaccess://"
-					+ this.nombre_bd, this.usuarioBD, this.passwordBD);
+			return DriverManager.getConnection(ruta, user, password);
+
+		} catch (ClassNotFoundException e) {
+			System.out.println("Error al cargar el driver");
+		 
+ 
 
 		} catch (SQLException e) {
-
-			JOptionPane.showMessageDialog(null,
-					"Error al realizar la conexion " + e);
-
-			return false;
-
+			System.out.println("Error en al conexion con XAMPP");
+		 
 		}
 
-		try {
+	 
 
-			this.sentencia = this.conexion.createStatement(
-			ResultSet.TYPE_SCROLL_INSENSITIVE,
-			ResultSet.CONCUR_READ_ONLY);
-
-		}
-
-		catch (SQLException e) {
-
-			JOptionPane.showMessageDialog(null,
-					"Error al crear el objeto sentencia " + e);
-
-			return false;
-
-		}
-
-		return true;
+		return null;
 
 	}
 
-	public ResultSet EjecutarSentencia(String sql) throws SQLException {
+	BufferedReader tcl = new BufferedReader(new InputStreamReader(System.in));
 
-		ResultSet rs;
-		rs = this.sentencia.executeQuery(sql);
-		return rs;
+	int matricula;
+	String nombre, grupo;
+	public boolean seEncontro;
+
+	public void altas() throws IOException {
+		Connection con = Conexion.getConnection();
+
+		try {
+			PreparedStatement pstmt = con
+					.prepareStatement("insert into productos(id_producto, nombre,presio,grupo)values(?,?,?)");
+
+			System.out.println("nombre del alumno:");
+			nombre = tcl.readLine();
+			System.out.println("matricula del alumno:");
+			matricula = Integer.parseInt(tcl.readLine());
+			System.out.println("grupo del alumno:");
+			grupo = tcl.readLine();
+
+			pstmt.setString(1, nombre);
+			pstmt.setInt(2, matricula);
+			pstmt.setString(3, grupo);
+			pstmt.executeUpdate();
+
+		} catch (SQLException e) {
+			System.out.println("ocurrio algun error");
+	
+
+		}
+
+	}
+
+	public void mostrar() {
+
+	}
+
+	public void borrar() {
+		Connection con = Conexion.getConnection();
+		try {
+			PreparedStatement pstmt = con
+					.prepareStatement("delete from alumnos where matricula=?");
+
+			pstmt.setString(1, nombre);
+			pstmt.setInt(2, matricula);
+			pstmt.setString(3, grupo);
+			pstmt.executeUpdate();
+			pstmt.executeUpdate();
+
+			pstmt.executeUpdate();
+
+		} catch (SQLException e) {
+			System.out.println("ocurrio un error");
+	 
+		}
+	}
+
+	public void modificar() {
+		Connection con = Conexion.getConnection();
+		try {
+			PreparedStatement pstmt = con
+					.prepareStatement("update alumnos set nombre, matricula=? where grupo");
+
+			pstmt.setString(1, nombre);
+			pstmt.setInt(2, matricula);
+			pstmt.setString(3, grupo);
+			pstmt.executeUpdate();
+
+		} catch (SQLException e) {
+			System.out.println("ocurrio un error");
+			 
+		}
+	}
+
+	public void consultas() throws IOException {
+		String aux;
+		Connection con = Conexion.getConnection();
+		try {
+			Statement st = con.createStatement();
+			System.out.println("digite la matricula del alumno a consultar");
+			aux = tcl.readLine();
+			matricula = Integer.parseInt(aux);
+			ResultSet tabla = st
+					.executeQuery("select * from alumnos where matricula="
+							+ matricula);
+			if (tabla.next()) {
+				System.out.println(" matricula: " + tabla.getInt("matricula")
+						+ "\n");
+				System.out.println("nombre: " + tabla.getString("nombre"));
+				System.out.println("grupo: " + tabla.getFloat("grupo") + "\n");
+
+			}
+		} catch (SQLException e) {
+			System.out.println("Error al consultar");
+		 
+		}
+	}
+
+ 
+	public void acceso(String codigoDeBarras) {
+
+		String aux;
+		Connection con = Conexion.getConnection();
+		try {
+			Statement st = con.createStatement();
+			System.out.println("si");
+			System.out.println(codigoDeBarras);
+
+			ResultSet tabla = st
+					.executeQuery("select * from admins where alumno_no_control =("
+							+ codigoDeBarras + ")");
+		 
+
+		} catch (SQLException s) {
+			System.out.println("Error al acceder");
+			 
+
+		}
+
+	}
+
+	static ResultSet registro = null;
+
+	public static void generarArrays() throws SQLException {
 
 	}
 
